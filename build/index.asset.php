c425a517f87fd462d7983887eac49067e81f05ba
Bharath <obharath@gmail.com>
2020-03-04 09:11:01 +0530

    cleanup

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c180223ecc6423562836bf525bfc0171');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b418bded4f15f82601ce90a5f9ca5054');
